636b8ec681ef7faf4d00c91e44add8b156340aec
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-11-20 21:22:49 +0000

    actually run commands

--- delete-vpc.sh
+++ delete-vpc.sh
@@ -23,19 +23,18 @@ for region in $(aws ec2 describe-regions --region eu-west-1 | jq -r .Regions[].R
   INTERNET_GATEWAY_ID=$(aws ec2 --region ${region} describe-internet-gateways --filter Name=attachment.vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .InternetGateways[0].InternetGatewayId)
   if [ "${INTERNET_GATEWAY_ID}" != "null" ]; then
     echo "Detaching and deleting internet gateway ${INTERNET_GATEWAY_ID}"
-    # aws ec2 --region ${region} detach-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID} --vpc-id ${DEFAULT_VPC_ID}
-    # aws ec2 --region ${region} delete-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID}
+    aws ec2 --region ${region} detach-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID} --vpc-id ${DEFAULT_VPC_ID}
+    aws ec2 --region ${region} delete-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID}
   fi
 
   SUBNETS=$(aws ec2 --region ${region} describe-subnets --filters Name=vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .Subnets[].SubnetId)
   if [ "${SUBNETS}" != "null" ]; then
     for subnet_id in ${SUBNETS}; do
       echo "Deleting subnet ${subnet_id}"
-      # aws ec2 --region ${region} delete-subnet --subnet-id ${subnet_id}
+      aws ec2 --region ${region} delete-subnet --subnet-id ${subnet_id}
     done
   fi
 
   echo "Deleting vpc ${DEFAULT_VPC_ID}"
-  # aws ec2 --region ${region} delete-vpc --vpc-id ${DEFAULT_VPC_ID}
-
+  aws ec2 --region ${region} delete-vpc --vpc-id ${DEFAULT_VPC_ID}
 done

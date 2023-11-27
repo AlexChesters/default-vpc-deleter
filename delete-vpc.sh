431788c97910bc402f63c30b9452996f535cfbfb
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-11-27 10:57:06 -0800

    update script

--- delete-vpc.sh
+++ delete-vpc.sh
@@ -10,31 +10,29 @@ export AWS_ACCESS_KEY_ID=$(echo $DELETER_ROLE_CREDENTIALS | jq -r .AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $DELETER_ROLE_CREDENTIALS | jq -r .SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $DELETER_ROLE_CREDENTIALS | jq -r .SessionToken)
 
-for region in $(aws ec2 describe-regions --region eu-west-1 | jq -r .Regions[].RegionName); do
-  echo "* Region ${region}"
+echo "* Region $REGION"
 
-  DEFAULT_VPC_ID=$(aws ec2 --region ${region} describe-vpcs --filter Name=isDefault,Values=true | jq -r .Vpcs[0].VpcId)
-  if [ "${DEFAULT_VPC_ID}" = "null" ]; then
-    echo "No default vpc found"
-    continue
-  fi
-  echo "Found default vpc ${DEFAULT_VPC_ID}"
+DEFAULT_VPC_ID=$(aws ec2 --region $REGION describe-vpcs --filter Name=isDefault,Values=true | jq -r .Vpcs[0].VpcId)
+if [ "${DEFAULT_VPC_ID}" = "null" ]; then
+  echo "No default vpc found"
+  continue
+fi
+echo "Found default vpc ${DEFAULT_VPC_ID}"
 
-  INTERNET_GATEWAY_ID=$(aws ec2 --region ${region} describe-internet-gateways --filter Name=attachment.vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .InternetGateways[0].InternetGatewayId)
-  if [ "${INTERNET_GATEWAY_ID}" != "null" ]; then
-    echo "Detaching and deleting internet gateway ${INTERNET_GATEWAY_ID}"
-    aws ec2 --region ${region} detach-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID} --vpc-id ${DEFAULT_VPC_ID}
-    aws ec2 --region ${region} delete-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID}
-  fi
+INTERNET_GATEWAY_ID=$(aws ec2 --region $REGION describe-internet-gateways --filter Name=attachment.vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .InternetGateways[0].InternetGatewayId)
+if [ "${INTERNET_GATEWAY_ID}" != "null" ]; then
+  echo "Detaching and deleting internet gateway ${INTERNET_GATEWAY_ID}"
+  aws ec2 --region $REGION detach-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID} --vpc-id ${DEFAULT_VPC_ID}
+  aws ec2 --region $REGION delete-internet-gateway --internet-gateway-id ${INTERNET_GATEWAY_ID}
+fi
 
-  SUBNETS=$(aws ec2 --region ${region} describe-subnets --filters Name=vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .Subnets[].SubnetId)
-  if [ "${SUBNETS}" != "null" ]; then
-    for subnet_id in ${SUBNETS}; do
-      echo "Deleting subnet ${subnet_id}"
-      aws ec2 --region ${region} delete-subnet --subnet-id ${subnet_id}
-    done
-  fi
+SUBNETS=$(aws ec2 --region $REGION describe-subnets --filters Name=vpc-id,Values=${DEFAULT_VPC_ID} | jq -r .Subnets[].SubnetId)
+if [ "${SUBNETS}" != "null" ]; then
+  for subnet_id in ${SUBNETS}; do
+    echo "Deleting subnet ${subnet_id}"
+    aws ec2 --region $REGION delete-subnet --subnet-id ${subnet_id}
+  done
+fi
 
-  echo "Deleting vpc ${DEFAULT_VPC_ID}"
-  aws ec2 --region ${region} delete-vpc --vpc-id ${DEFAULT_VPC_ID}
-done
+echo "Deleting vpc ${DEFAULT_VPC_ID}"
+aws ec2 --region $REGION delete-vpc --vpc-id ${DEFAULT_VPC_ID}
